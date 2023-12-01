10c7c41b2012437b3fbfcfb66c7dec8db59705fe
mdengra9 <mireia.den.esp@gmail.com>
2023-12-01 11:49:59 +0100

    vamooos

---
+++ Assets/Scripts/Rayo.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rayo : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetButtonDown("Fire1")) 
+       {
+        RaycastTest();
+       }
+    }
+
+    void RaycastTest()
+    {
+        Ray ray =Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if(hit.transform.gameObject.layer == 6)
+            {
+                Debug.Log(hit.transform.name);
+            }
+            else if(hit.transform.gameObject.layer == 7)
+            {
+                Debug.Log(hit.transform.name);
+            }
+            else if(hit.transform.gameObject.layer == 8)
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+}
